47d38d7e77b8808aeeacf157d598de72ab7ea447
Abdulai Bawa Mashud <abdulaibawam@gmail.com>
2020-11-12 15:15:03 +0000

    push

---
+++ matrix_calculator.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+void matrix_A(){
+int A[3][3];
+
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        cout<< "please Enter the values of matrix A: "<< i+1 << j+1 << endl;
+        cin>> A[i][j];
+        
+    }
+}
+}
+
+void matrix_B(){
+
+int B[3][3];
+for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+        cout<< "please Enter the values of matrix A: "<< i+1 << j+1 << endl;
+        cin>> B[i][j];
+        
+    }
+}
+}
+
+int main (){
+
+}
